e7d016bfe721f235ae44eb72bef9ec2901a92efa
Mellany Linhares <mellinhares6@gmail.com>
2020-03-17 16:13:43 -0300

    Update cadastrar_sistema_planetario.dart

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ void main(){
           "/cadastrar_sistema_estrela" : (context) => cadastrar_sistema_estrela(),
           "cadastrar_sistema_planeta" : (context) => cadastrar_sistema_planeta()
         },
-        home: login(),
+        home: cadastrar_sistema_planetario(),
         theme: ThemeData(
             primaryColor: Colors.deepPurple,
             accentColor: Colors.deepPurpleAccent,
