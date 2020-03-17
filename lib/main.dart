b977ab984b3755084c01848a07084374f3d936a9
Mellany Linhares <mellinhares6@gmail.com>
2020-03-17 18:36:01 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ import 'package:universo_bd/planeta/listar_planeta.dart';
 import 'package:universo_bd/login.dart';
 import 'package:universo_bd/relacionamentos/sistema_estrela/cadastrar_sistema_estrela.dart';
 import 'package:universo_bd/relacionamentos/sistema_planeta/cadastrar_sistema_planeta.dart';
+import 'package:universo_bd/relacionamentos/sistema_planeta/listar_sistema_planeta.dart';
 import 'package:universo_bd/satelite_natural/cadastrar_satelite_natural.dart';
 import 'package:universo_bd/satelite_natural/editar_satelite_natural.dart';
 import 'package:universo_bd/satelite_natural/exibir_satelite_natural.dart';
@@ -57,7 +58,8 @@ void main(){
           "/exibir_sistema_planetario" : (context) => exibir_sistema_planetario(arguments: ModalRoute.of(context).settings.arguments),
           "/editar_sistema_planetario" : (context) => editar_sistema_planetario(arguments: ModalRoute.of(context).settings.arguments),
           "/cadastrar_sistema_estrela" : (context) => cadastrar_sistema_estrela(),
-          "cadastrar_sistema_planeta" : (context) => cadastrar_sistema_planeta()
+          "/cadastrar_sistema_planeta" : (context) => cadastrar_sistema_planeta(),
+          "/listar_sistema_planeta" : (context) => listar_sistema_planeta()
         },
         home: login(),
         theme: ThemeData(
