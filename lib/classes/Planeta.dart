b44bce53cb8aa67ba37c09805d6ae42c060d5740
Eduardo Amorim <atenas503@gmail.com>
2020-03-20 00:07:50 -0300

    Add delete method.

--- lib/classes/Planeta.dart
+++ lib/classes/Planeta.dart
@@ -1,4 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:universo_bd/classes/Galaxia.dart';
+import 'package:universo_bd/classes/SistemaPlanetario.dart';
 
 class Planeta{
 
@@ -45,6 +48,40 @@ class Planeta{
 
   set id(String value) {
     _id = value;
+  }
+
+  void deletarPlaneta() async{
+    Firestore db = Firestore.instance;
+    db.collection("planetas").document(id).delete();
+    await db.collection("sistemas_planetas").where("idPlaneta", isEqualTo: id).getDocuments().then((snapshot){
+      for(DocumentSnapshot item in snapshot.documents){
+        db.collection("sistemas_planetarios").document(item.data["idSistema"]).get().then((snapshotSP){
+          SistemaPlanetario sp = SistemaPlanetario();
+          var dados = snapshotSP.data;
+          Galaxia galaxia = Galaxia();
+          galaxia.id = dados["idGalaxia"];
+          sp.id=item.data["idSistema"];
+          sp.idade=dados["idade"];
+          sp.qtdPlanetas=dados["qtdPlanetas"];
+          sp.qtdEstrelas=dados["qtdEstrelas"];
+          sp.nome = dados["nome"];
+          sp.galaxia = galaxia;
+          sp.qtdPlanetas--;
+          sp.editarSistemaPlanetario(sp.galaxia);
+        });
+        db.collection("sistemas_planetas").document(item.documentID).delete();
+      }
+    });
+    await db.collection("orbitantes").where("idPlaneta", isEqualTo: id).getDocuments().then((snapshot){
+      for(DocumentSnapshot item in snapshot.documents){
+        db.collection("orbitantes").document(item.documentID).delete();
+      }
+    });
+    Fluttertoast.showToast(
+      msg: "Planeta deletado com sucesso!",
+      toastLength: Toast.LENGTH_LONG,
+      gravity: ToastGravity.BOTTOM,
+    );
   }
 
   void editarPlaneta(){
