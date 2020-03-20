4935840d3e681c98f343a1610569aaf267415214
Mellany Linhares <mellinhares6@gmail.com>
2020-03-19 22:25:27 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,12 @@ import 'package:universo_bd/planeta/listar_planeta.dart';
 import 'package:universo_bd/login.dart';
 import 'package:universo_bd/relacionamentos/menu_relacoes.dart';
 import 'package:universo_bd/relacionamentos/sistema_estrela/cadastrar_sistema_estrela.dart';
+import 'package:universo_bd/relacionamentos/sistema_estrela/editar_sistema_estrela.dart';
+import 'package:universo_bd/relacionamentos/sistema_estrela/exibir_sistema_estrela.dart';
+import 'package:universo_bd/relacionamentos/sistema_estrela/listar_sistema_estrela.dart';
 import 'package:universo_bd/relacionamentos/sistema_planeta/cadastrar_sistema_planeta.dart';
+import 'package:universo_bd/relacionamentos/sistema_planeta/editar_sistema_planeta.dart';
+import 'package:universo_bd/relacionamentos/sistema_planeta/exibir_sistema_planeta.dart';
 import 'package:universo_bd/relacionamentos/sistema_planeta/listar_sistema_planeta.dart';
 import 'package:universo_bd/satelite_natural/cadastrar_satelite_natural.dart';
 import 'package:universo_bd/satelite_natural/editar_satelite_natural.dart';
@@ -38,13 +43,13 @@ void main(){
           "/cadastrar_planeta" : (context) => cadastrar_planeta(),
           "/editar_planeta" : (context) => editar_planeta(planeta: ModalRoute.of(context).settings.arguments),
           "/listar_planeta" : (context) => listar_planeta(),
-          "/exibir_planeta" : (context) => exibir_planeta(planeta: ModalRoute.of(context).settings.arguments),
+          "/exibir_planeta" : (context) => exibir_planeta(arguments: ModalRoute.of(context).settings.arguments),
           "/cadastrar_estrela" : (context) => cadastrar_estrela(tipo_estrela: ModalRoute.of(context).settings.arguments),
           "/listar_estrela" : (context) => listar_estrela(),
           "/editar_estrela" : (context) => editar_estrela(estrela: ModalRoute.of(context).settings.arguments),
-          "/exibir_estrela" : (context) => exibir_estrela(estrela: ModalRoute.of(context).settings.arguments),
+          "/exibir_estrela" : (context) => exibir_estrela(arguments: ModalRoute.of(context).settings.arguments),
           "/cadastrar_gigante_vermelha" : (context) => cadastrar_gigante_vermelha(),
-          "/exibir_gigante_vermelha" : (context) => exibir_gigante_vermelha(estrela: ModalRoute.of(context).settings.arguments),
+          "/exibir_gigante_vermelha" : (context) => exibir_gigante_vermelha(arguments: ModalRoute.of(context).settings.arguments),
           "/editar_gigante_vermelha" : (context) => editar_gigante_vermelha(estrela: ModalRoute.of(context).settings.arguments),
           "/listar_galaxia" : (context) => listar_galaxia(),
           "/cadastrar_galaxia" : (context) => cadastrar_galaxia(),
@@ -52,15 +57,20 @@ void main(){
           "/editar_galaxia" : (context) => editar_galaxia(galaxia: ModalRoute.of(context).settings.arguments),
           "/cadastrar_satelite_natural" : (context) => cadastrar_satelite_natural(),
           "/listar_satelite_natural" : (context) => listar_satelite_natural(),
-          "/exibir_satelite_natural" : (context) => exibir_satelite_natural(sateliteNatural: ModalRoute.of(context).settings.arguments),
+          "/exibir_satelite_natural" : (context) => exibir_satelite_natural(arguments: ModalRoute.of(context).settings.arguments),
           "/editar_satelite_natural" : (context) => editar_satelite_natural(sateliteNatural: ModalRoute.of(context).settings.arguments),
           "/cadastrar_sistema_planetario" : (context) => cadastrar_sistema_planetario(),
           "/listar_sistema_planetario" : (context) => listar_sistema_planetario(),
-          "/exibir_sistema_planetario" : (context) => exibir_sistema_planetario(sistemaPlanetario: ModalRoute.of(context).settings.arguments),
+          "/exibir_sistema_planetario" : (context) => exibir_sistema_planetario(arguments: ModalRoute.of(context).settings.arguments),
           "/editar_sistema_planetario" : (context) => editar_sistema_planetario(sistemaPlanetario: ModalRoute.of(context).settings.arguments),
           "/cadastrar_sistema_estrela" : (context) => cadastrar_sistema_estrela(),
+          "/listar_sistema_estrela" : (context) => listar_sistema_estrela(),
+          "/exibir_sistema_estrela" : (context) => exibir_sistema_estrela(sistemaEstrela: ModalRoute.of(context).settings.arguments),
+          "/editar_sistema_estrela" : (context) => editar_sistema_estrela(sistemaEstrela: ModalRoute.of(context).settings.arguments),
           "/cadastrar_sistema_planeta" : (context) => cadastrar_sistema_planeta(),
+          "/editar_sistema_planeta" : (context) => editar_sistema_planeta(sistemaPlaneta: ModalRoute.of(context).settings.arguments),
           "/listar_sistema_planeta" : (context) => listar_sistema_planeta(),
+          "/exibir_sistema_planeta" : (context) => exibir_sistema_planeta(sistemaPlaneta: ModalRoute.of(context).settings.arguments),
           "/menu_relacoes" : (context) => menu_relacoes(),
         },
         home: login(),
